7ad8aa8a1fe0f1ddb4bd6428b6cc622f96ab14e6
rustytaco <84538339+PripyatAutomations@users.noreply.github.com>
2021-10-01 18:44:48 +0000

    work on hamlib stubs

--- hamlib.c
+++ hamlib.c
@@ -11,6 +11,164 @@
 #include <hamlib/rotator.h>
 #include "mod_hamradio.h"
 
+switch_status_t	radio_hamlib_set_freq() {
+//      rig_set_freq
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_freq() {
+//      rig_get_freq();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t	radio_hamlib_set_mode() {
+//	rig_set_mode();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_mode() {
+//	rig_get_mode();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_ptt() {
+//	rig_set_ptt();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_ptt() {
+//	rig_get_ptt();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_ctcss() {
+//	rig_set_ctcss_tone();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_ctcss() {
+//	rig_get_ctcss_tone();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_ctcss_sql() {
+//	rig_set_ctcss_sql();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_ctcss_sql() {
+//	rig_get_ctcss_sql();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_dcs() {
+//	rig_set_dcs_tone();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_dcs() {
+//	rig_get_dcs_tone();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_dcs_sql() {
+//	rig_set_dcs_sql();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_dcs_sql() {
+//	rig_get_dcs_sql();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_vfo() {
+//	rig_set_vfo();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_vfo() {
+//	rig_get_vfo();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_vfo_mode() {
+//	rig_get_vfo_mode();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+/////////////////////////////////
+// repeater/split offset stuff //
+/////////////////////////////////
+switch_status_t radio_hamlib_set_rptr_shift() {
+//	rig_set_rptr_shift();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_rptr_shift() {
+//	rig_get_gptr_shift();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_rptr_offset() {
+//	rig_set_rptr_offs();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_rptr_offset() {
+//	rig_get_gptr_offs();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_split_freq() {
+//	rig_set_split_freq();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_split_freq() {
+//	rig_get_split_freq();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_split_freq_mode() {
+//	rig_set_split_freq_mode();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_split_freq_mode() {
+//	rig_get_split_freq_mode();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_split_vfo() {
+//	rig_set_split_vfo();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_split_vfo() {
+//	rig_get_split_vfo();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_rit() {
+//	rig_set_rit();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_rit() {
+//	rig_get_rit();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_set_xit() {
+//	rig_set_xit();
+      return SWITCH_STATUS_SUCCESS;
+}
+
+switch_status_t radio_hamlib_get_xit() {
+//	rig_get_xit();
+      return SWITCH_STATUS_SUCCESS;
+}
+
 // Initialize the connection to hamlib for the selected radio
 switch_status_t radio_hamlib_init_radio(const int radio) {
     Radio_t *r = NULL;
--- hamlib.h
+++ hamlib.h
@@ -1,8 +1,44 @@
 #if	!defined(RADIO_RIGCTL_H)
 #define	RADIO_RIGCTL_H
 
+#include <switch.h>
+#include <hamlib/rig.h>
+
 extern switch_status_t radio_hamlib_init_radio(const int radio);
 extern switch_status_t radio_hamlib_init(void);
 extern switch_status_t radio_hamlib_fini(void);
+
+///
+extern switch_status_t	radio_hamlib_set_freq();
+extern switch_status_t radio_hamlib_get_freq();
+extern switch_status_t	radio_hamlib_set_mode();
+extern switch_status_t radio_hamlib_get_mode();
+extern switch_status_t radio_hamlib_set_ptt();
+extern switch_status_t radio_hamlib_get_ptt();
+extern switch_status_t radio_hamlib_set_ctcss();
+extern switch_status_t radio_hamlib_get_ctcss();
+extern switch_status_t radio_hamlib_set_ctcss_sql();
+extern switch_status_t radio_hamlib_get_ctcss_sql();
+extern switch_status_t radio_hamlib_set_dcs();
+extern switch_status_t radio_hamlib_get_dcs();
+extern switch_status_t radio_hamlib_set_dcs_sql();
+extern switch_status_t radio_hamlib_get_dcs_sql();
+extern switch_status_t radio_hamlib_set_vfo();
+extern switch_status_t radio_hamlib_get_vfo();
+extern switch_status_t radio_hamlib_get_vfo_mode();
+extern switch_status_t radio_hamlib_set_rptr_shift();
+extern switch_status_t radio_hamlib_get_rptr_shift();
+extern switch_status_t radio_hamlib_set_rptr_offset();
+extern switch_status_t radio_hamlib_get_rptr_offset();
+extern switch_status_t radio_hamlib_set_split_freq();
+extern switch_status_t radio_hamlib_get_split_freq();
+extern switch_status_t radio_hamlib_set_split_freq_mode();
+extern switch_status_t radio_hamlib_get_split_freq_mode();
+extern switch_status_t radio_hamlib_set_split_vfo();
+extern switch_status_t radio_hamlib_get_split_vfo();
+extern switch_status_t radio_hamlib_set_rit();
+extern switch_status_t radio_hamlib_get_rit();
+extern switch_status_t radio_hamlib_set_xit();
+extern switch_status_t radio_hamlib_get_xit();
 
 #endif	// !defined(RADIO_RIGCTL_H)
