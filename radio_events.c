f554f1d82db58145079f0847e39c3c24a70e82df
rustytaco <84538339+PripyatAutomations@users.noreply.github.com>
2021-10-02 00:39:44 +0000

    cleanup

--- radio_events.c
+++ radio_events.c
@@ -8,6 +8,15 @@
 #include "mod_hamradio.h"
 
 static void radio_reload_configuration(switch_event_t *evt) {
+   // Exclude some excessively noisy, yet useless events
+   if ((evt->event_id == SWITCH_EVENT_HEARTBEAT) ||
+       (evt->event_id == SWITCH_EVENT_RE_SCHEDULE) ||
+       (evt->event_id == SWITCH_EVENT_API) ||
+       (evt->event_id == SWITCH_EVENT_LOG) ||
+       (evt->event_id == SWITCH_EVENT_STARTUP) ||
+       (evt->event_id == SWITCH_EVENT_SHUTDOWN))
+      return;
+
    radio_load_configuration(true);
 }
 
@@ -15,7 +24,7 @@ static void radio_reload_configuration(switch_event_t *evt) {
 static void radio_cry_event(switch_event_t *evt) {
    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "--Unhandled Event--\n");
    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Key: %lu Flags: %i\n", evt->key, evt->flags);
-   switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "ID: %06i Subclass: %s Priority: %03i \n", evt->event_id, evt->subclass_name, evt->priority);
+   switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "ID: %s Subclass: %s Priority: %03i \n", switch_event_name(evt->event_id), evt->subclass_name, evt->priority);
    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Owner: %s\n", evt->owner);
    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "bind_user_data: %p, event_user_data: %p\n", evt->bind_user_data, evt->event_user_data);
    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "--Unhandled Event--\n");
@@ -27,6 +36,10 @@ static RadioEvent_t radio_events[] = {
    ////////////////////
    // Audio activity //
    ////////////////////
+
+   // log all events... This is extremely verbose and likely only useful for developers ;)
+   { SWITCH_EVENT_ALL, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
+#if	0
    // Speech activity detected
    { SWITCH_EVENT_DETECTED_SPEECH, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
    // Detected tones (not DTMF?)
@@ -37,9 +50,11 @@ static RadioEvent_t radio_events[] = {
    { SWITCH_EVENT_TALK, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
    // Audio has ended
    { SWITCH_EVENT_NOTALK, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
+#endif
    // Reload the configuration
    { SWITCH_EVENT_RELOADXML, SWITCH_EVENT_SUBCLASS_ANY, radio_reload_configuration },
 
+#if	0
    ////////////////////
    // Channel Events //
    ////////////////////
@@ -50,6 +65,7 @@ static RadioEvent_t radio_events[] = {
    { SWITCH_EVENT_CHANNEL_BRIDGE, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
    { SWITCH_EVENT_CHANNEL_UNBRIDGE, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
    { SWITCH_EVENT_CHANNEL_HANGUP, SWITCH_EVENT_SUBCLASS_ANY, radio_cry_event },
+#endif
 
    // Terminating element - Do not modify.
    { 0, NULL, NULL }
